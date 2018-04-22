dc9491ce04f03404713ce501a4e21bfa4c51429d
Glenna <35232055+glennaf@users.noreply.github.com>
2018-04-22 10:41:13 -0400

    Update and rename graphs.R to visuals.R

--- R/graphs.R
+++
@@ -1,7 +0,0 @@
-#GRAPHS
-
-boxplot(CFB11_16$`Winning Total Yards`~CFB11_16$`Losing Total Yards`,
-        xlab="Losing Total Yards",
-        ylab="Winning Total Yards",
-        main="Winning vs. Losing Yards")
-
