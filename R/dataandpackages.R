e4630421b5fb071bf456f2092fbe5e65f1ac2230
Glenna <35232055+glennaf@users.noreply.github.com>
2018-04-27 22:58:49 -0400

    Add files via upload

---
+++ R/dataandpackages.R
@@ -0,0 +1,5 @@
+#@title Data and Packages
+#@description This code will load the data file and useful packages.
+
+CFB11_16 <- read.csv("https://raw.githubusercontent.com/keefeseanc/dataMining/master/CFB%202011%202016%20-%20Sheet1.csv")
+#source: https://raw.githubusercontent.com/keefeseanc/dataMining/master/CFB%202011%202016%20-%20Sheet1.csv
