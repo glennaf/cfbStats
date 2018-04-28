6a676d998ab9a774f0378ce6e4ca4f3d1afa4033
Glenna <35232055+glennaf@users.noreply.github.com>
2018-04-27 22:58:28 -0400

    Add files via upload

---
+++ R/visuals.R
@@ -0,0 +1,46 @@
+#@title VISUALS
+#description Visual analytics representative of the data.
+
+summary(CFB11_16)
+
+#HISTOGRAMS
+
+#Winning Points
+hist(CFB11_16$'Winning Points',
+     main="Histogram for Winning Points",
+     xlab="Winning Points",
+     ylab="Frequency",
+     border="black",
+     col="tan")
+
+#Losing Points
+hist(CFB11_16$`Losing Points`,
+     main="Histogram for Losing Points",
+     xlab="Losing Points",
+     ylab="Frequency",
+     border="black",
+     col="tan")
+
+#Winning Yards
+hist(CFB11_16$`Winning Total Yards`,
+     main="Histogram for Winning Yards",
+     xlab="Winning Yards",
+     ylab="Frequency",
+     border="black",
+     col="dark green")
+
+#Losing Yards
+hist(CFB11_16$`Losing Total Yards`,
+     main="Histogram for Losing Yards",
+     xlab="Losing Yards",
+     ylab="Frequency",
+     border="black",
+     col="dark green")
+
+
+#BOXPLOT
+boxplot(CFB11_16$`Winning Total Yards`~CFB11_16$`Losing Total Yards`,
+        xlab="Losing Total Yards",
+        ylab="Winning Total Yards",
+        main="Winning vs. Losing Yards")
+
